63002f454e35a3ad8f6036f880939a3dedb489d6
Alejandro Ordonez <alejandroordonezp@gmail.com>
2025-03-07 08:43:52 +0100

    Arreglo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule),
-    canActivate: [!AuthGuard]
+    
   },
   {
     path: 'animales',
